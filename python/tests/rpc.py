3fe22b5fa2e4dffab52c1c0df6864759163412f9
Jakub Klama <jakub@ixsystems.com>
2016-07-31 19:26:57 -0700

    Add few streaming RPC tests.

--- python/tests/rpc.py
+++ python/tests/rpc.py
@@ -24,3 +24,76 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 #####################################################################
+
+import os
+import socket
+import unittest
+import logging
+from freenas.dispatcher.rpc import RpcService, generator
+from freenas.dispatcher.client import Client, StreamingResultIterator
+
+
+class TestService(RpcService):
+    def hello(self, arg):
+        return 'Hello World, {0}'.format(arg)
+
+    @generator
+    def iterator(self, count):
+        return (i * 2 for i in range(0, count))
+
+    @generator
+    def maybe_iterator(self, value):
+        pass
+
+    @generator
+    def generator(self, count):
+        pass
+
+
+class TestClientServer(unittest.TestCase):
+    def setup_back_to_back(self, streaming=False):
+        a, b = socket.socketpair()
+        self.assertGreaterEqual(a.fileno(), 0)
+        self.assertGreaterEqual(b.fileno(), 0)
+
+        c1 = Client()
+        c1._s = a
+        c1.enable_server()
+        c1.standalone_server = True
+
+        if streaming:
+            c1.streaming = True
+            c1.rpc.streaming_enabled = True
+        c1.register_service('test', TestService())
+        c1.connect('fd://{0}'.format(a.fileno()))
+        self.assertTrue(c1.connected)
+
+        c2 = Client()
+        c2._s = b
+        c2.streaming = True
+        c2.connect('fd://{0}'.format(b.fileno()))
+        self.assertTrue(c2.connected)
+
+        return c1, c2
+
+    def test_hello(self):
+        c1, c2 = self.setup_back_to_back()
+        result = c2.call_sync('test.hello', 'freenas')
+        self.assertEqual(result, 'Hello World, freenas')
+
+    def test_iterator(self):
+        c1, c2 = self.setup_back_to_back(True)
+        result = c2.call_sync('test.iterator', 10)
+        self.assertIsInstance(result, StreamingResultIterator)
+        self.assertEqual(list(result), [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+
+    def test_iterator_compat(self):
+        c1, c2 = self.setup_back_to_back(False)
+        result = c2.call_sync('test.iterator', 10)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    unittest.main()
